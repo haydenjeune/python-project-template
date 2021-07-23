7e68c387e0608ed7903b11463ee8be6c3a1e8b29
Hayden Jeune <33794706+haydenjeune@users.noreply.github.com>
2021-07-24 10:30:35 +1200

    Add pyright and format files correctly

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
+
 def parse_reqs(filename):
     with open(filename) as f:
         lines = f.readlines()
         no_space = [l.strip() for l in lines]
         reqs = [r for r in no_space if r and not r.startswith("#")]
     return reqs
+
 
 setup(
     name="{{cookiecutter.project_name}}",
@@ -14,7 +16,5 @@ setup(
     python_requires=">={{cookiecutter.python_version}}",
     packages=find_packages(),
     install_requires=parse_reqs("requirements.txt"),
-    extras_require={
-        "dev": parse_reqs("requirements.dev.txt")
-    }
+    extras_require={"dev": parse_reqs("requirements.dev.txt")},
 )
